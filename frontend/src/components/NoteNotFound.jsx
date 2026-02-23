429e9bccd1f607c79bf8c8a61562efb1bf918acc
Divyadharshini <divyadharshinind.1020@gmail.com>
2026-02-23 11:37:42 +0530

    Initial commit

---
+++ frontend/src/components/NoteNotFound.jsx
@@ -0,0 +1,9 @@
+const NoteNotFound = () =>{
+    return (
+        <div className="text-center py-10">
+            <h2 className=" text-xl font-semibold">No Notes Found</h2>
+            <p className="epacity-70">create your first note📝</p>
+        </div>
+    )
+}
+export default NoteNotFound;
---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,9 @@
+export const formatDate= (date) => {
+ if (!date) return "";
+
+    return new Date(date).toLocaleDateString("en-IN", {
+        year: "numeric",
+        month: "short",
+        day: "2-digit",
+    });
+};
